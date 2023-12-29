6cf605bee8ddfb02d44cef8fb10f2c4e9930e700
FerBermudez <blfernando.fb@gmail.com>
2023-12-28 22:24:46 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+//midelwares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.get('/api', (req, res) => {
+    return res.json({ hello: 'dog' });
+});
+
+app.get('/api/marco', (req, res) => {
+    return res.end("polo");
+});
+
+app.listen(3000, () => {
+    console.log('Server on port 3000')
+});
